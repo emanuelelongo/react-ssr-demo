7df3726bd5f5b805435d41129e9404d48105f12f
Emanuele Longo <longo.emanuele@gmail.com>
2017-12-12 22:02:58 +0100

    High order component to fetch data

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
-export default class Home extends React.Component {
+// -------- suppose this to be in another file ---------------
+const hoc = loader => Component => {
+  return class WrappedComponent extends React.Component {
+    
+    componentWillMount() {
+      // damn... this will not wait to data to be loaded!
+      loader(this)
+    }
+
+    render() {
+      return <Component {...this.state}/>
+    }
+  }
+}
+// ------------------------------------------------------------
+
+class Home extends React.Component {
+
   render() {
     return (
       <div>
         <h2>~ Home Page ~</h2>
+        <span>Benvenuto {this.props.name}</span>
       </div>
     );
   }
 }
+
+const loader = (component) => {
+  return new Promise((resolve) => 
+    setTimeout(() => {
+      component.setState({name: 'Emanuele'})
+      resolve()
+    }, 1000)
+  )
+}
+
+export default hoc(loader)(Home)
